d7370e2c9297e521af42b8d1718e96cf2b6a9ff0
bince_deloitte <betul.ince@rabobank.nl>
2024-06-05 10:44:40 +0200

    builder pattern example

---
+++ chapter-01-creating-and-destroying-objects/src/NutritionFactsBuilderClass.java
@@ -0,0 +1,90 @@
+/***
+ * the builder pattern is a good choice when designing classes whose constructors or static factories would have more
+ * than a handful of parameters.
+ *
+ * Pizza(int size) { ... }
+ * Pizza(int size, boolean cheese) { ... }
+ * Pizza(int size, boolean cheese, boolean pepperoni) { ... }
+ * Pizza(int size, boolean cheese, boolean pepperoni, boolean bacon) { ... }
+ *
+ * this is called the telescoping constructor pattern, the problem is that once constructors are 4 or 5 parameters long
+ * it becomes difficult to remember the required order of the parameters as well as what particular constructor you
+ * might want in a given situation. One alternative solution is JavaBeans pattern where you call  a constructor with the
+ * mandatory params and then call any optional setters after:
+ *
+ * Pizza pizza = new Pizza(12);
+ * pizza.setCheese(true);
+ * pizza.setPepperoni(true);
+ * pizza.setBacon(true);
+ *
+ * The better alternative is to use BuilderPattern:
+ */
+public class NutritionFactsBuilderClass {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        // Required parameters
+        private final int servingSize;
+        private final int servings;
+
+        // Optional parameters - initialized to default values
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val) {
+            calories = val;
+            return this;
+        }
+
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val) {
+            sodium = val;
+            return this;
+        }
+
+        public Builder carbohydrate(int val) {
+            carbohydrate = val;
+            return this;
+        }
+
+        public NutritionFactsBuilderClass build() {
+            return new NutritionFactsBuilderClass(this);
+        }
+    }
+
+    private NutritionFactsBuilderClass(Builder builder) {
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+
+    public static void main(String[] args) {
+        NutritionFactsBuilderClass cocaCola = new Builder(240, 8)
+                .calories(100)
+                .sodium(35)
+                .carbohydrate(27)
+                .build();
+
+        System.out.println("Serving size: " + cocaCola.servingSize);
+        System.out.println("The amount of sodium that coca cola has: " + cocaCola.sodium);
+    }
+}
--- chapter-01-creating-and-destroying-objects/src/StaticFactoryMethodExampleClass.java
+++ chapter-01-creating-and-destroying-objects/src/StaticFactoryMethodExampleClass.java
@@ -1,6 +1,21 @@
+public class StaticFactoryMethodExampleClass {
 
-public class Main {
+    public static Boolean valueOf(boolean b) {
+        // Static factory method which is simply a static method that returns an instance of the class
+        return b ? Boolean.TRUE : Boolean.FALSE;
+        // This method converts a primitive boolean value to its corresponding Boolean object
+        // If b is true, it returns Boolean.TRUE
+    }
+
+    // A class can provide its clients with static methods instead of public constructors
+    // Static methods, unlike constructors, don't require creating a new object each time they're invoked
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean b = true;
+
+        Boolean result = StaticFactoryMethodExampleClass.valueOf(b);
+        Class<? extends Boolean> resultType = result.getClass();
+
+        System.out.println("Type of result: " + resultType.getName());
     }
 }
