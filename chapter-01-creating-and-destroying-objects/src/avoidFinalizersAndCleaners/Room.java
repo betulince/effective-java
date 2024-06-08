4772bd41fbf6880dfb40aac605a2a055e64f3bb6
betulince <btl.ince98@gmail.com>
2024-06-09 00:00:09 +0200

    add comments

--- chapter-01-creating-and-destroying-objects/src/avoidFinalizersAndCleaners/Room.java
+++ chapter-01-creating-and-destroying-objects/src/avoidFinalizersAndCleaners/Room.java
@@ -2,14 +2,20 @@ package avoidFinalizersAndCleaners;
 
 import java.lang.ref.Cleaner;
 
+/**
+ * Let's assume that rooms must be cleaned before they are reclaimed.
+ */
 public class Room implements AutoCloseable {
     private static final Cleaner cleaner = Cleaner.create();
 
+    // Resource that requires cleaning. Must not refer to Room!
     private static class State implements Runnable {
         int numJunkPiles;
         State(int numJunkPiles) {
             this.numJunkPiles = numJunkPiles;
         }
+
+        // Invoked by close method or cleaner
         @Override
         public void run() {
             System.out.println("Cleaning room");
@@ -17,7 +23,9 @@ public class Room implements AutoCloseable {
         }
     }
 
+    // The state of this room, shared with our cleanable
     private final State state;
+    // Our cleanable. Cleans the room when it's eligible for garbage collection
     private final Cleaner.Cleanable cleanable;
 
     public Room(int numJunkPiles) {
